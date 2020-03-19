4e4728d4a996a2d6da0ef07191f69cc0ff112645
ikenfin <ikenfin@gmail.com>
2020-03-19 16:56:15 +0400

    added sentry environment

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const { runParsers } = require('./util')
 const Sentry = require('@sentry/node')
 
 Sentry.init({
-  dsn: process.env.SENTRY_DSN
+  dsn: process.env.SENTRY_DSN,
+  environment: process.env.SENTRY_ENVIRONMENT
 })
 
 const saveToDB = (data) => {
