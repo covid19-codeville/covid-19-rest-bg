27541ad538c8a4f2b33fa903b750d4cbb2fb4846
ikenfin <ikenfin@gmail.com>
2020-04-01 15:03:01 +0400

    logger interface

---
+++ logger.js
@@ -0,0 +1,27 @@
+const Sentry = require('@sentry/node')
+const winston = require('winston')
+
+let logger = null
+
+if (process.env.NODE_ENV !== 'development') {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    environment: process.env.SENTRY_ENVIRONMENT
+  })
+
+  logger = Sentry
+}
+else {
+  const winstonLogger = winston.createLogger({
+    format: winston.format.json(),
+    transports: [
+      new winston.transports.File({ filename: 'combined.log' })
+    ]
+  })
+
+  logger = {
+    captureException: winstonLogger.error
+  }
+}
+
+module.exports = logger
